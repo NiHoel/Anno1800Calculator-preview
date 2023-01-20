6bcb5b2e17b046d18216407d6bb8845df5c8375f
Nico Höllerich <s1nihoel@uni-bayreuth.de>
2023-01-20 18:20:42 +0100

    Fix overproduction.

--- js/factories.js
+++ js/factories.js
@@ -376,7 +376,7 @@ export class Factory extends Consumer {
             this.buildings.subscribe(val => this.workforceDemand.updateAmount(Math.max(val, this.buildings())));
 
 
-        this.overProduction = ko.pureComputed(() => Math.max(0, this.externalProduction() - this.inputAmountByOutput(), (this.inputAmountByExistingBuildings() - this.inputAmountByOutput()) * this.extraGoodFactor()));
+        this.overProduction = ko.pureComputed(() => Math.max(0, this.inputAmount() * this.extraGoodFactor() + this.externalProduction() - this.totalDemands()));
         if(this.extraGoodProductionList)
             this.extraGoodsDisplayAmount = ko.pureComputed(() => this.extraGoodProductionList.checked() ? this.extraGoodProductionList.nonZero().reduce((a, b) => a + b.amount(), 0) : 0)
 
